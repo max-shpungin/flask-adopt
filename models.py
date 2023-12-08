da680bcdd920eabfa3a49609243b95f99db8005f
Max Shpungin <maxsdance@gmail.com>
2023-12-07 18:53:50 -0800

    started Pet class

--- models.py
+++ models.py
@@ -5,6 +5,23 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+
+####################HELPERS##########################
+
 def connect_db(app):
     """Connect this database to provided Flask app.
 
